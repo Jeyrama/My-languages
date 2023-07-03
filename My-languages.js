af1be0a6079cdc9d10416191a2476128b2909aef
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-03 14:43:43 -0700

    Update My-languages.js

--- My-languages.js
+++ My-languages.js
@@ -20,3 +20,13 @@ const myLanguages = results => Object.entries(results)
   .map(([name, points]) => name);
 
 // or
+
+function myLanguages(results) {
+  let arr = [];
+  for (let key in results) {
+    if (results[key] >= 60) {
+      arr.push(key)
+    }
+  }
+  return arr.sort((a, b) => results[b] - results[a])
+}
