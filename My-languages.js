4c3feaa3edf8d178702c23297b44cb9813b4569f
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-03 14:35:41 -0700

    Update My-languages.js

--- My-languages.js
+++ My-languages.js
@@ -12,7 +12,11 @@ Examples:
 */
 
 
+// Solution
+
 const myLanguages = results => Object.entries(results)
   .filter(([name, points]) => points >= 60)
   .sort(([name1, points1], [name2, points2]) => points2 - points1)
   .map(([name, points]) => name);
+
+// or
